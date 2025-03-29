42d1c4305f520a38584d57c435601aabb4971370
narumi <toucans-cutouts0f@icloud.com>
2025-03-29 13:40:15 +0800

    ✨ feat(auth): implement MAXExchange for fetching index prices and markets

--- src/mcp_server_max/types/market.py
+++ src/mcp_server_max/types/market.py
@@ -1,13 +1,14 @@
 from pydantic import BaseModel
+from pydantic import Field
 
 
 class Market(BaseModel):
-    id: str
-    status: str
-    base_unit: str
-    base_unit_precision: int
-    min_base_amount: float
-    quote_unit: str
-    quote_unit_precision: int
-    min_quote_amount: float
-    m_wallet_supported: bool
+    id: str = Field(..., alias="symbol", description="unique market id")
+    status: str = Field(..., description="market status")
+    base_unit: str = Field(..., description="base unit")
+    base_unit_precision: int = Field(..., description="fixed precision of base unit")
+    min_base_amount: float = Field(..., description="minimum of base amount")
+    quote_unit: str = Field(..., description="quote unit")
+    quote_unit_precision: int = Field(..., description="fixed precision of quote unit")
+    min_quote_amount: float = Field(..., description="minimum of quote amount")
+    m_wallet_supported: bool = Field(..., description="m-wallet supported")
