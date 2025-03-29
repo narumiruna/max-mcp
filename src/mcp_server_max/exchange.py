7f3632b0e49c52b3c5c4ddf199ad4e7ec2372e6e
narumi <toucans-cutouts0f@icloud.com>
2025-03-29 16:10:20 +0800

    get ticker

--- src/mcp_server_max/exchange.py
+++ src/mcp_server_max/exchange.py
@@ -1,6 +1,7 @@
 from .client import MAXRestClient
 from .types import Currency
 from .types import Market
+from .types import Ticker
 
 
 class MAXExchange:
@@ -38,3 +39,23 @@ class MAXExchange:
         """
         resp = await self.client.make_request("/api/v3/wallet/m/index_prices")
         return {k: float(v) for k, v in resp.items()}
+
+    async def get_tickers(self) -> dict[str, Ticker]:
+        """Get latest tickers of all markets
+
+        Returns:
+            list[Ticker]: A list of latest tickers.
+        """
+        resp = await self.client.make_request("/api/v2/tickers")
+        return {k: Ticker.model_validate(v | {"market": k}) for k, v in resp.items()}
+
+    async def get_ticker(self, symbol: str) -> Ticker:
+        """Get latest ticker of a specific market
+
+        Args:
+            symbol (str): The market symbol to get the ticker for.
+        Returns:
+            Ticker: The latest ticker of the specified market.
+        """
+        resp = await self.client.make_request("/api/v3/ticker", params={"market": symbol})
+        return Ticker.model_validate(resp | {"market": symbol})
--- src/mcp_server_max/types/__init__.py
+++ src/mcp_server_max/types/__init__.py
@@ -2,3 +2,4 @@ from .currency import Currency
 from .currency import Network
 from .currency import Staking
 from .market import Market
+from .ticker import Ticker
