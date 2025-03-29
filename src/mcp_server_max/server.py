cde309fa4e287fbb8b9268ac5abed0f88db1b3e3
narumi <toucans-cutouts0f@icloud.com>
2025-03-29 13:57:22 +0800

    ✨ feat(exchange): add get_markets method to retrieve available markets

--- src/mcp_server_max/server.py
+++ src/mcp_server_max/server.py
@@ -3,18 +3,19 @@ from typing import Annotated
 from mcp.server.fastmcp import FastMCP
 from pydantic import Field
 
-from mcp_server_max.v3.
+from .exchange import MAXExchange
+
+exchange = MAXExchange()
+
 # https://github.com/jlowin/fastmcp/issues/81#issuecomment-2714245145
 mcp = FastMCP("MCP Server MAX", log_level="ERROR")
 
 
 @mcp.tool()
-def add_numbers(
-    a: Annotated[float, Field(description="The first number")],
-    b: Annotated[float, Field(description="The second number")],
-) -> str:
-    """Add two numbers and return the result as a string."""
-    return f"{a} + {b} = {a+b} from MCP MAX"
+async def get_markets() -> str:
+    """Get all available markets."""
+    markets = await exchange.get_markets()
+    return "\n".join(str(market) for market in markets)
 
 
 def main():
